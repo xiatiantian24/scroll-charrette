0c6276f410562ce21ce46fa93a536844eb04501c
Ray Xia <xia.t@wustl.edu>
2024-02-21 13:06:16 -0600

    rename to index

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 document.getElementById('navbar').innerHTML = `
     <ul>
-      <li><a href="1.html">1:</a></li>
+      <li><a href="index.html">1:</a></li>
       <li><a href="2.html">2:</a></li>
       <li><a href="3.html">3:</a></li>
       <li><a href="4.html">4:</a></li>
